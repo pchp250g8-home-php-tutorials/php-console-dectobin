80e132f07cb27b0c1e901b7fec62b30f744a5875
Alexander <pchp250g8@tutamail.com>
2026-02-19 19:53:24 +0300

    Created proect DecToBin

---
+++ DecToBin.php
@@ -0,0 +1,22 @@
+<?php
+    echo("Input an unsigned integer number\r\n");
+    $uNum = intval(chop(fgets(STDIN)));
+    if(($uNum < 0) or ($uNum > PHP_INT_MAX))
+    {
+        echo("Invalid number format");
+        exit(0);
+    }
+    $uTempVal = $uNum;
+    $strBinNum ="";
+    while ($uTempVal > 0)
+    {
+        $strBinNum = ($uTempVal % 2) . $strBinNum;
+        $uTempVal = intdiv($uTempVal, 2);
+    }
+    if(strlen($strBinNum) == 0)
+    {
+        $strBinNum = "0";
+    }
+    printf("The binary equavalent of the number %d is %s\r\n", $uNum, $strBinNum);
+    fgetc(STDIN);
+?>
